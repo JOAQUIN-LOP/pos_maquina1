0f3cc5b2920bb321f6559ea7e7338c9ce1c049e8
Vett <vett10010@gmail.com>
2018-02-16 21:31:15 -0600

    added -creacion de seeders

---
+++ database/seeds/inv_sucursalSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class inv_sucursalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
