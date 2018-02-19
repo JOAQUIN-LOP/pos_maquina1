7e31bb414cb701d906e414282a2dd702207b79d5
viktor <viktoraju@gmail.com>
2018-02-18 19:20:50 -0600

    ADDED, models, path \App

---
+++ app/Empresa.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class Empresa extends Model
+{
+    use SoftDeletes;
+    protected $table = 'empresa';
+
+    protected $fillable = array('nom_empresa','direccion', 'ACTIVO');                                                                          
+
+    protected $hidden = ['deleted_at', 'created_at','updated_at'];
+
+    protected $dates = ['deleted_at'];
+}
