5a36e564fbd79e91320bb1ba3c23e81702dc92cf
viktor <viktoraju@gmail.com>
2018-02-22 10:39:09 -0600

    UPDATED, HomeController.php - updating by redirecto to dashboard after login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -56,7 +56,7 @@ class HomeController extends Controller
 
             if(Auth::attempt($userdata)){
 
-                echo 'SUCCES!';
+                return Redirect::to('home');
             
             }else{
 
