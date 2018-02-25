71bfe3c7e1bce5b4f55bbd96a0b8877f75115474
viktor <viktoraju@gmail.com>
2018-02-25 12:57:46 -0600

    ADDED, data_products.blade.php - added new file for data_table products view

---
+++ resources/views/data_products.blade.php
@@ -0,0 +1,11 @@
+@extends('adminlte::page')
+
+@section('title', 'Listado Productos')
+
+@section('content_header')
+    <h1>Listado de Productos</h1>
+@stop
+
+@section('content')
+    <p>Información General</p>
+@stop
