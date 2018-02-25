c0e46ae3d24a13bd52cd30385fc1001f5709e076
Gpobregonn <guillermo@foxylabs.gt>
2018-02-25 10:31:03 -0600

    [ADDED] se creo la vista articulos.balde.php con su formulario

---
+++ resources/views/articulos.blade.php
@@ -0,0 +1,79 @@
+@extends('adminlte::page')
+
+@section('title', 'Articulos')
+
+@section('content_header')
+    <h1>Articulos</h1>
+@stop
+
+@section('content')
+
+<!-- Form Element sizes -->
+<div class="box box-success">
+  <div class="box-header with-border">
+    <h3 class="box-title">Crear Articulo</h3>
+  </div>
+  <!-- box-body -->
+  <div class="box-body">
+    
+    <div class="row form-group">
+      <div class="col-xs-1">
+        <label>Producto</label>
+      </div>
+      <div class="col-xs-5">
+        <input type="text" class="form-control .input-lg" placeholder=".col-xs-5">
+      </div>
+      <div class="col-xs-1">
+        <label>Unidad</label>
+      </div>
+      <div class="col-xs-5">
+        <input type="text" class="form-control .input-lg" placeholder=".col-xs-5">
+      </div>
+    </div>  
+    
+    <div class="row form-group">
+      <div class="col-xs-1">
+        <label>Precio Compra</label>
+      </div>
+      <div class="col-xs-5">
+        <input type="text" class="form-control .input-lg" placeholder=".col-xs-5">
+      </div>
+      <div class="col-xs-1">
+        <label>Existencia</label>
+      </div>
+      <div class="col-xs-5">
+        <input type="text" class="form-control .input-lg" placeholder=".col-xs-5">
+      </div>
+    </div>  
+
+    <div class="row form-group altura-30">
+      <div class="col-xs-1">
+        <label>Tipo</label>
+      </div>
+      <div class="col-xs-5">
+        <input type="text" class="form-control .input-lg" placeholder=".col-xs-5">
+      </div>
+      <div class="col-xs-1">
+        <label>No. Inventario</label>
+      </div>
+      <div class="col-xs-5">
+        <input type="text" class="form-control .input-lg" placeholder=".col-xs-5">
+      </div>
+    </div>
+
+    <div class="row form-group altura-30">
+      <div class="col-xs-1">
+        <label>Estado</label>
+      </div>
+      <div class="col-xs-5">
+        <label>
+                  <input type="radio" name="r1" class="minimal">
+                </label>
+      </div>
+    </div>
+
+  </div>
+  <!-- /.box-body -->
+</div>
+    
+@stop
