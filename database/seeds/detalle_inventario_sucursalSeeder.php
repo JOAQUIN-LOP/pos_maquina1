319a5dfb600a6dee1a5c5eb7e56157f71e698199
Vett <vett10010@gmail.com>
2018-03-01 17:23:06 -0600

    creacion de archivos seeders: detalle_producto, tabla_calculo_sucursal, tabla_calculo, detalle_inventario, detalle_inventario_sucursal

---
+++ database/seeds/detalle_inventario_sucursalSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class detalle_inventario_sucursalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
---
+++ database/seeds/tabla_calculo_sucursalSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class tabla_calculo_sucursalSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
