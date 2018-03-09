e96ccd60e1a71692cfbe7078f667754d1fef9c6a
Vett <vett10010@gmail.com>
2018-03-08 20:57:56 -0600

    se modifico el seeder detalle inventario

--- database/seeds/detalle_inventarioSeeder.php
+++ database/seeds/detalle_inventarioSeeder.php
@@ -18,8 +18,8 @@ class detalle_inventarioSeeder extends Seeder
             'mes'                            => '02',
             'anio'                           => '2018',
             'fecha'                          => '2018-02-28',
-        	'cantidad_total'		         => '1000',
-        	'subtotal_inventario' 		     => '8000',
+        	'cant_total'		             => '1000.00',
+        	'subtotal_inventario' 		     => '8000.00',
             'created_at' 			         => date('Y-m-d H:m:s'),
             'updated_at' 			         => date('Y-m-d H:m:s') 
         )); 
