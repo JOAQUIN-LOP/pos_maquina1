ff786eeaafa06ca58efaf88e07add3016f3a185c
viktor <viktoraju@gmail.com>
2018-04-03 19:56:33 -0600

    UPDATED, DetalleFactura.php,Factura.php - adding new functions

--- app/DetalleFactura.php
+++ app/DetalleFactura.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class DetalleFactura extends Model
 {
-    //
+	use SoftDeletes;
+
+	protected $table = 'detalle_factura';
+	protected $fillable = array('idFactura','idProducto', 'cantidad','precio_unit','total_venta');
+
+	protected $hidden = ['deleted_at', 'created_at','updated_at'];
+
+	protected $dates = ['deleted_at'];
+
 }
--- app/Factura.php
+++ app/Factura.php
@@ -3,8 +3,17 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquen\SofttDeletes;
 
 class Factura extends Model
 {
-    //
+	use SoftDeletes;
+
+	protected $table = 'factura';
+
+	protected $fillable = array('num_factura','idEmpresa', 'idSucursal','idUsuario', 'dia', 'mes', 'anio', 'fecha','hora', 'direccion','total_factura','estado');
+
+    protected $hidden = ['deleted_at', 'created_at','updated_at'];
+
+    protected $dates = ['deleted_at'];
 }
