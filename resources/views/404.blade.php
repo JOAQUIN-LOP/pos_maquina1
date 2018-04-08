2f12ececbec4f82f4584461dc37ef518b46d2858
Gpobregonn <guillermo@foxylabs.gt>
2018-04-08 13:27:25 -0600

    [UPDATED] se cambio la libreria datatable y se agrego ajax

---
+++ resources/views/404.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <section class="error-container">
+            eroor
+            <span class="four"><span class="screen-reader-text">4</span></span>
+            <span class="zero"><span class="screen-reader-text">0</span></span>
+            <span class="four"><span class="screen-reader-text">4</span></span>
+        </section>
+    </body>
+</html>
--- resources/views/DetalleArticulos.blade.php
+++ resources/views/DetalleArticulos.blade.php
@@ -8,192 +8,174 @@
 
 @section('content')
 
-<!-- Form Element sizes -->
 <div class="box box-success">
 
   <div class="box-header with-border">
     <h3 class="box-title">Detalle Articulo</h3>
   </div>
-  <div class="alert alert-success">
-    <strong>Success!</strong> {{Session::get('flash_message')}}
-  </div> 
-  @endif
+  <input type="text" value="{{URL::to('/home/detalle/precio')}}" id="pathDetalleProd" class="form-control" style="display:none">
+  <input type="text" value="{{URL::to('/home/producto')}}" id="pathProd" class="form-control" style="display:none">
   <!-- box-body -->
     <div class="box-body">
-      {{--  inicio form  --}}
-      <form action="{{URL::to('/home/detalle/precio')}}" method="POST">
-        {{ csrf_field() }}      
-        {{--  inicio row  --}}
-        <div class="row">
-          <div class="col-sm-3">
-            <div class="form-group">
-              <div class="input-group">
-                <span class="input-group-addon font-bolder">Id</span>
-                <input type="text" class="form-control" name="IdProducto" id="IdProducto" readonly required>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-3">
-            <div class="form-group">
-              <div class="input-group">
-                <span class="input-group-addon font-bolder">Producto</span>
-                <input type="text" class="form-control" name="nomProducto" id="nomProducto" readonly required>
-              </div>
-            </div>
-          </div>
-          <div class="col-sm-3">
-              <div class="form-group">
-                <div class="input-group">
-                  <span class="input-group-addon font-bolder">mes</span>
-                  <select class="form-control" name="mesDetalle" id="mesDetalle">
-                      <option value="">Seleccione Mes</option>  
-                      <option value="1">Enero</option>
-                      <option value="2">Febrero</option>
-                      <option value="3">Marzo</option>
-                      <option value="4">Abril</option>
-                      <option value="5">Mayo</option>
-                      <option value="6">Junio</option>
-                      <option value="7">Julio</option>
-                      <option value="8">Agosto</option>
-                      <option value="9">Septiembre</option>
-                      <option value="10">Octubre</option>
-                      <option value="11">Noviembre</option>
-                      <option value="12">Diciembre</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-3">
-                <div class="form-group">
-                  <div class="input-group">
-                    <span class="input-group-addon font-bolder">Año</span>
-                    <select class="form-control" name="AnioDetalle" id="AnioDetalle">
-                        <option value="">Seleccione Anio</option>  
-                    </select>
-                  </div>
-                </div>
-              </div>
-        </div>
-        {{--  fin row  --}}
-        <div class="row top-row">
-            <div class="col-sm-3">
-                <div class="form-group">
-                  <div class="input-group">
-                    <span class="input-group-addon font-bolder">Cantidad</span>
-                      <input type="text" class="form-control OnlyNumber" name="cantidad_unidades" id="cantidad_unidades" required>
-                  </div>
-                </div>
-              </div>
-            <div class="col-sm-3">
-              <div class="form-group">
-                <div class="input-group">
-                  <span class="input-group-addon font-bolder">precio total</span>
-                  <input type="text" class="form-control" name="precio_total_compras" id="precio_total_compras" required>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-1">
-              <div class="form-group">
-                <input type ="button" id="calcular" class="btn btn-primary" value="Calcular">
-              </div>
-            </div>
-            <div class="col-sm-2">
-              <div class="form-group">
-                <div class="input-group">
-                  <span class="input-group-addon font-bolder">precio unidad</span>
-                  <input type="text" class="form-control" name="precio_unidad" id="precio_unidad" readonly required>
-                </div>
-              </div>
-            </div>
-            <div class="col-sm-3 text-center">
-              <div class="form-group">
-                <button type ="submit" id="calcular" class="btn btn-success"><i class="fa fa-save"></i> Guardar</button>
-              </div>
-            </div>
-        </div>
-      </form>
-      {{--  fin form  --}}
+
       {{--  inici box body  --}}
-        <div class="box-body">
-          <div class="row">
-            <div class="col-sm-12">
-              <table id="tableProduct" class="display" >
-                <thead>
-                <tr>
-                  <th>id</th>
-                  <th>Producto</th>
-                  <th>Descripcion</th>
-                  <th>Estado</th>
-                </tr>
-                </thead>
-                <tbody>
-                  @foreach($producto as $prod) 
-                    @if ( $prod->estado == 1)
-                      <tr class="fila">
-                        <td>{{ $prod->id }}</td>
-                        <td>{{ $prod->nomProducto }}</td>
-                        <td>{{ $prod->descripcion_producto }}</td>
-                        <td>
-                          @if ($prod->estado == 1)
-                            <span class="label label-success"> Activo </span>
-                          @endif                    
-                        </td>
-                      </tr> 
-                    @endif                 
-                  @endforeach
-                </tbody>
-              </table>
-            </div>
-          </div>
-          {{--  fin box body  --}}
-      </div>
-      <div class="row">
-        <div class="col-sm-12">
-          <h3 class="box-title">Detalle Articulo</h3>
-        </div>
-      </div>
-      <div class="container">
+      <div class="box-body">
         <div class="row">
-          <table id="detalleProducto" class="display" >
+          <div class="col-sm-12">
+            <table id="TablaDetalle" class="display" >
             <thead>
-            <tr>
-              <th>Producto</th>
-              <th>Cantidad</th>
-              <th>Precio Unitario</th>
-              <th>Precio Total</th>
-              <th>Mes</th>
-              <th>Anio</th>
-              <th>estado</th>
-            </tr>
+              <tr>
+                <th>id</th>
+                <th>Producto</th>
+                <th>Descripcion</th>
+                <th>Estado</th>
+                <th>Opcion</th>
+              </tr>
             </thead>
             <tbody>
-              @foreach($detalleproducto as $dtprod) 
-              <tr>
-                <td>{{ $dtprod->producto->nomProducto }}</td>
-                <td>{{ $dtprod->cantidad_unidades }}</td>
-                <td>{{ $dtprod->precio_unidad }}</td>
-                <td>{{ $dtprod->precio_total_compras }}</td>
-                <td>{{ $dtprod->mes }}</td>
-                <td>{{ $dtprod->anio }}</td>
-                <td>
-                  @if ( $dtprod->estado == 1)
-                    <span class="label label-success"> Activo </span>
-                  @endif                    
-                </td>
-              </tr>                
-              @endforeach
             </tbody>
           </table>
         </div>
-        {{--  fin box body  --}}
+      </div>
+      {{--  fin box body  --}}
     </div>
 
+    {{--  /box-body   --}}
+    <div class="modal modal-info fade" id="modal-info">
+        <div class="modal-dialog modal-xl">
+          <div class="modal-content ">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span></button>
+              <h4 class="modal-title">Crear Precio Producto</h4>
+            </div>
+            <div class="modal-body">
+              
+                <form id="FormPrecioProd" action="{{URL::to('/home/detalle/precio')}}" method="POST">
+                  {{ csrf_field() }}   
+                  <input type="text" class="form-control" name="_token" id="token" value="{{ csrf_token() }}" style="display:none">   
+                  {{--  inicio row  --}}
+                  <div class="row">
+                    <div class="col-sm-3">
+                      <div class="form-group">
+                          <label for="exampleInputPassword1">Id Producto</label>
+                          <input type="text" class="form-control" name="IdProducto" id="IdProducto" readonly required>
+                      </div>
+                    </div>
+                    <div class="col-sm-3">
+                      <div class="form-group">
+                          <label for="exampleInputPassword1">Producto</label>
+                          <input type="text" class="form-control" name="nomProducto" id="nomProducto" readonly required>
+                      </div>
+                    </div>
+                    <div class="col-sm-3">
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Mes</label>
+                            <select class="form-control" name="mesDetalle" id="mesDetalle">
+                                <option value="">Seleccione Mes</option>  
+                                <option value="1">Enero</option>
+                                <option value="2">Febrero</option>
+                                <option value="3">Marzo</option>
+                                <option value="4">Abril</option>
+                                <option value="5">Mayo</option>
+                                <option value="6">Junio</option>
+                                <option value="7">Julio</option>
+                                <option value="8">Agosto</option>
+                                <option value="9">Septiembre</option>
+                                <option value="10">Octubre</option>
+                                <option value="11">Noviembre</option>
+                                <option value="12">Diciembre</option>
+                            </select>
+                        </div>
+                      </div>
+                      <div class="col-sm-3">
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Año</label>
+                              <select class="form-control" name="AnioDetalle" id="AnioDetalle">
+                                  <option value="">Seleccione Anio</option>  
+                              </select>
+                          </div>
+                        </div>
+                  </div>
+                  {{--  fin row  --}}
+                  <div class="row top-row">
+                      <div class="col-sm-3">
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Cantidad</label>
+                                <input type="text" class="form-control OnlyNumber" name="cantidad_unidades" id="cantidad_unidades" required>
+                          </div>
+                        </div>
+                      <div class="col-sm-3">
+                        <div class="form-group">
+                            <label for="exampleInputPassword1">Precio Total</label>
+                              <input type="text" class="form-control OnlyNumbreFloat" name="precio_total_compras" id="precio_total_compras" required>
+                        </div>
+                      </div>
+                      <div class="col-sm-1">
+                        <div class="form-group" style="padding-top:25px">
+                          <input type ="button" id="calcular" class="btn btn-outline" value="Calcular">
+                        </div>
+                      </div>
+                      <div class="col-sm-2">
+                          <div class="form-group">
+                            <label for="exampleInputPassword1">Precio U</label>
+                            <input type="text" class="form-control" name="precio_unidad" id="precio_unidad" readonly required>
+                          </div>
+                      </div>
+                      <div class="col-sm-3 text-center">
+                        <div class="form-group" style="padding-top:25px"s>
+                          <button type ="submit" id="GuardarPrecio" class="btn btn-outline"><i class="fa fa-save"></i> Guardar</button>
+                        </div>
+                      </div>
+                  </div>
+                </form>
+                
+                <hr>
+               <div class="container-fluid">
+                    <div class="table-responsive">
+                      <table id="detallePrecioProducto" class="table" >
+                        <thead>
+                        <tr>
+                          <th>Producto</th>
+                          <th>Cantidad</th>
+                          <th>Precio Unitario</th>
+                          <th>Precio Total</th>
+                          <th>Mes</th>
+                          <th>Anio</th>
+                          <th>estado</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                  
+                        </tbody>
+                      </table>
+                    </div>
+                    {{--  fin box body  --}}
+                    
+                    <div class="alert" id="notification-container" style="display:none;">
+                        <div class="notification">
+                            <button class="notification-close"></button>
+                            <div class="notification-title"><span id="titulo"></span> !</div>
+                            <div class="notification-message"><span id="mensaje"></span></div>
+                        </div>
+                    </div>
+                                        
+                </div>
 
-
-    </div>
-  <!-- /.box-body -->
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">Salir</button>
+            </div>
+          </div>
+          <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+      </div>
 </div>
-
-@if(Session::has('flash_message'))
-
 @stop
+@section('js')
+  <script type="text/javascript" src="{{ asset('js/detalleArticulos.js') }}"></script>
+  @stack('js')
+  @yield('js')
+@stop
+
