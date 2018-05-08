38a341df0dd6d3d1951445655db2f4665291063a
Gpobregonn <guillermo@foxylabs.gt>
2018-05-07 23:12:58 -0600

    95% terminado

--- public/js/detalleInventario.js
+++ public/js/detalleInventario.js
@@ -4,10 +4,6 @@ $('document').ready(function(){
     var url = $('#CrearInventario').attr('action');
     var idInv = null;
 
-    var All = $('#All').DataTable( {
-        responsive: true
-    });
-
     let meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
     let NumInventario = [1,2,3,4,5,6,7,8,9,10,11,12];
     
@@ -15,27 +11,21 @@ $('document').ready(function(){
     var mes = fecha.getMonth();
     var anio = fecha.getFullYear();
 
+    var AllProd = $('#AllProd').DataTable( {
+        responsive: true,
+        "lengthMenu": [5],
+    });
+    
+    var All;
+
     $.get(url+'/home/detalle/inventario/ver/activo', headers = { 'X-CSRF-TOKEN': token }, function (result) {
         idInv = result[0].idInventario;
         $("#idInventario").val(result[0].idInventario);
         $("#empresa").val(result[0].empresa);
         $("#anio").val(result[0].anio);
         $("#mes").append("<option value='"+NumInventario[result[0].mes - 1]+"'>"+meses[result[0].mes - 1]+"</option>");
-
-        $.get(url+'/home/detalle/inventario/'+idInv, headers = { 'X-CSRF-TOKEN': token }, function (result) {
-            $(result).each(function (key, value) {
-                All.row.add([
-                    key+1,
-                    value['producto'],
-                    meses[value['mes']-1],
-                    value['anio'],
-                    value['sub'],
-                    value['cant']
-                ]).draw(false);
-                $( ".odd" ).addClass("fila");
-                $( ".even" ).addClass("fila");
-            })
-        });
+        
+        AllInv(idInv);
     });
 
     $.get(url+'/home/producto', headers = { 'X-CSRF-TOKEN': token }, function (result) {
@@ -44,17 +34,11 @@ $('document').ready(function(){
         });
     });
 
-    var AllProd = $('#AllProd').DataTable( {
-        responsive: true,
-        "lengthMenu": [5],
-    });
-    
 
     $("#CodProducto").change(function(){
         id = $("#CodProducto").val();
         cargarProd(id);
     });
-
 
     function cargarProd(id){
         AllProd.rows().remove().draw();
@@ -69,7 +53,7 @@ $('document').ready(function(){
                     parseInt(value['cantidad_unidades']),
                     '<input type="number" class="form-control input-sm OnliNumber cantidad" max="'+parseInt(value['cantidad_unidades'])+'" value="" onkeypress="return event.charCode >= 48" min="0">',
                     '<input type="text" class="form-control input-sm total" readonly>',
-                    '<a name="'+value['id_detalle_producto']+'" class="btn btn-primary btn-xs Agregar" data-toggle="tooltip" title="Agregar Producto" style="display:none"><i class="fa fa-plus"></i></a>'
+                    '<a name="'+value['idProducto']+'" class="btn btn-primary btn-xs Agregar" data-toggle="tooltip" title="Agregar Producto" style="display:none"><i class="fa fa-plus"></i></a>'
                 ]).draw(false);
                 $( ".odd" ).addClass("fila");
                 $( ".even" ).addClass("fila");
@@ -78,6 +62,31 @@ $('document').ready(function(){
 
     };
 
+    function AllInv(){
+        
+        All = $('#All').DataTable( {
+            responsive: true,
+            destroy: true,
+            "ajax": {
+                url: url+'/home/detalle/inventario/'+idInv,
+                type : "get",
+                success: function(result){
+                    $(result).each(function (key, value) {
+                        All.row.add([
+                            key+1,
+                            value['producto'],
+                            meses[value['mes']-1],
+                            value['anio'],
+                            value['sub'],
+                            value['cant']
+                        ]).draw(false);
+                        $( ".odd" ).addClass("fila");
+                        $( ".even" ).addClass("fila");
+                    })
+                }
+            },
+        });    
+    }
 
     $('#AllProd').on('blur','tr.fila input.cantidad', function(){
         
@@ -132,24 +141,40 @@ $('document').ready(function(){
                     data: data,
                     success: function (response) {
                         if (response['notification'] == "success") {
-                            $('#mensaje').text(' Se Creo Inventario No. '+ response['data']+' Exitosamente ');
-                          }
-                          if (response['notification'] == "danger") {
-                            $('#mensaje').html(objeto.message + "<br>" + response.data  + "<br> No existe");
-                          }
-                          if (response['notification'] == "warning") {
-                            objeto = response["data"];
-                            $('#mensaje').html( response.data  + "<br> error de servidor interno");
-                          }
-                          $('#modal-warning').modal('toggle');
-                          // notificacion
-                          id = $("#CodProducto").val();
-                          cargarProd(id);
-                          $('div#notification-container').fadeIn(350);
-                          $(".notification").addClass("notification-"+response['notification']);
-                          $('#titulo').text(response['notification']);
-            
-                          $('div#notification-container').delay(3000).fadeOut(350);
+                        $('#mensaje').text(' Se Creo Inventario No. '+ response['data']+' Exitosamente ');
+                        }
+                        if (response['notification'] == "danger") {
+                        $('#mensaje').html(objeto.message + "<br>" + response.data  + "<br> No existe");
+                        }
+                        if (response['notification'] == "warning") {
+                        objeto = response["data"];
+                        $('#mensaje').html( response.data  + "<br> error de servidor interno");
+                        }
+                        $('#modal-warning').modal('toggle');
+                        // notificacion
+                        All.clear();
+                        id = $("#CodProducto").val();
+                        cargarProd(id);
+                        $.get(url+'/home/detalle/inventario/'+idInv, headers = { 'X-CSRF-TOKEN': token }, function (result) {
+                            $(result).each(function (key, value) {
+                                All.row.add([
+                                    key+1,
+                                    value['producto'],
+                                    meses[value['mes']-1],
+                                    value['anio'],
+                                    value['sub'],
+                                    value['cant']
+                                ]).draw(false);
+                                $( ".odd" ).addClass("fila");
+                                $( ".even" ).addClass("fila");
+                            })
+                        });
+    
+                        $('div#notification-container').fadeIn(350);
+                        $(".notification").addClass("notification-"+response['notification']);
+                        $('#titulo').text(response['notification']);
+        
+                        $('div#notification-container').delay(3000).fadeOut(350);
                     }
                 });
                 
@@ -157,7 +182,9 @@ $('document').ready(function(){
         
     });
 
+
     $("#modalS").click(function(){
+        $('#CodProducto').val("").trigger('change.select2');
         $("#modalSuccess").modal()
         AllProd.rows().remove().draw();
     })
--- resources/views/DetalleInventario.blade.php
+++ resources/views/DetalleInventario.blade.php
@@ -120,6 +120,15 @@
         <button type="button" class="btn btn-outline pull-left" data-dismiss="modal"><i class="fa fa-close"></i> Cerrar</button>
       </div>
     </div>
+  
+  <div class="alert" id="notification-container" style="display:none;">
+    <div class="notification">
+        <button class="notification-close"></button>
+        <div class="notification-title"><span id="titulo"></span> !</div>
+        <div class="notification-message"><span id="mensaje"></span></div>
+    </div>
+  </div>
+
     <!-- /.modal-content -->
   </div>
   <!-- /.modal-dialog -->
@@ -145,13 +154,6 @@
     </div>
   </div>
 
-</div>
-<div class="alert" id="notification-container" style="display:none;">
-    <div class="notification">
-        <button class="notification-close"></button>
-        <div class="notification-title"><span id="titulo"></span> !</div>
-        <div class="notification-message"><span id="mensaje"></span></div>
-    </div>
 </div>
 @stop
 @section('js')
