2488a481f835ebeb97e0819edb1880052b8184c4
viktor <viktoraju@gmail.com>
2018-05-28 00:57:10 -0600

    UPDATED, web.php - updating by adding POST routes for cargar_factura

--- routes/web.php
+++ routes/web.php
@@ -29,7 +29,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::post('/home/datos_factura/{id}', 'FacturaController@see');
 Route::post('/home/detalle_factura/{id}','FacturaController@detalles');
 Route::get('/home/crear/factura', 'FacturaController@formFactura');
-Route::get('/home/crear/carga_factura', 'FacturaController@cargaFactura');
+Route::post('/home/crear/carga_factura/{id}', 'FacturaController@cargaFactura');
 Route::resource('/home/factura', 'FacturaController');
 
 // Empresa
