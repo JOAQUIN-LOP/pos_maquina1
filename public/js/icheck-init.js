590772ad50665e101dcd02339c75407af2456544
Gpobregonn <guillermo@foxylabs.gt>
2018-06-10 14:25:05 -0600

    [updated]

--- public/js/icheck-init.js
+++ public/js/icheck-init.js
@@ -1,9 +1,8 @@
-<script>
+
 $ (document) .ready (function () {
   $ ('input'). iCheck ({
-    casilla de verificación Clase: 'icheckbox_square',
+    Clase: 'icheckbox_square',
     radioClass: 'iradio_square',
     increaseArea: '20% ' // opcional
   });
 });
-</ script>
